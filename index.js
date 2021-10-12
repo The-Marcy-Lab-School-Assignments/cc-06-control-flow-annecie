c706d985316c2e1213a59a9e0c4ab515cec461fb
annecie <ajbexcelsior@gmail.com>
2021-10-12 14:22:59 +0000

    problem 1 and 2 completed

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 //1
 function sumOfThreeAndFive(){
-  
+  let sum=0    
+  for (let i=1; i<1000; i++)
+  if(i%3 ===0 && i%5 === 0){
+   sum+=i   
+  }
+}
+return sum
 }
 
-function greaterNum(){
-  
+function greaterNum(num1, num2){
+  if(num1===num2){
+ return"both intergers are equal"
+  }else if(num1>num2)
+  return num1 
+}else if(num2>num1){
+  }else{
+     return null
+   }
 }
 
 function oddAndEvenInFifteen(){
@@ -14,7 +27,6 @@ function oddAndEvenInFifteen(){
 function sortThreeNums(){
   
 }
-
 
 
 
